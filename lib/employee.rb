1a39ce27204151c570d0595f60410b9d3ab8ab11
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-23 11:03:53 -0700

    Add validation to Employee class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,14 @@
+=begin
+  - Employees must always have a first name present
+  - Employees must always have a last name present
+  - Employees have a hourly_rate that is a number (integer) between 40 and 200
+  - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+=end
+
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
 end
