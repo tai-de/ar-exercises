19f01436811a02eb34ee17e0a9d08cd167f90035
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-23 10:02:16 -0700

    Update exercise_3.rb

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,14 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+=begin
+1. Load the third store (into @store3) as you did the other two before.
+2. Using Active Record's destroy method, delete the store from the database.
+3. Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
+=end
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count
