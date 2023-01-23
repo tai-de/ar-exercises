719a2e3b2889ba1ac6d3447c2b3ab36cef5196a5
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-01-23 09:57:38 -0700

    Update exercise_2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+=begin
+1. Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+2. Load the second store from the database and assign it to @store2.
+3. Update the first store (@store1) instance in the database. (Change its name or something.)
+=end
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "West Burnaby")
+
+pp Store.find_by(id: 1)
